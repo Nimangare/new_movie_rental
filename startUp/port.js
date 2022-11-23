44c4fad490f5daccbaa2d9629ed4df590e9683d0
Nitin Nimangare <nitin.nimangare22@gmail.com>
2022-11-23 14:38:54 +0530

    final

--- startUp/port.js
+++ startUp/port.js
@@ -1,7 +1,7 @@
 const config = require("config");
 
 module.exports = function (app) {
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5001;
   app.listen(PORT, () => {
     console.log(`Server is listening on port:${PORT}`);
   });
